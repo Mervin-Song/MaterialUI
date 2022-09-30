d56aab8bba868bc0fe5e549d4a39515a6ccaace4
MERVIN <mervinsong.wj@gmail.com>
2022-09-30 15:25:34 +0800

    feat: added remove function to pet cards | edit function is not working

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,103 +1,112 @@
 const Data = [
-        {
-          type: "dog",
-          name: "Bruno",
-          breed: "Border Collie",
-          img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg",
-          description: "qweqweqweqweqweqwe",
-          vaccinationStatus: true,
-          vaccinationTypes: { rabies: false, mother: false, dad: true },
-          medicalRecords:
-            "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
-        },
-        {
-          type: "cat",
-          name: "Bruce",
-          breed: "Maine Coon",
-          img: "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x2.jpg",
-          description: "qweqweqweqweqweqwe",
-          vaccinationStatus: false,
-          vaccinationTypes: { rabies: false, mother: false, dad: true },
-          medicalRecords:
-            "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
-        },
-        {
-          type: "dog",
-          name: "Tyson",
-          breed: "Bulldog",
-          img: "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403",
-          description: "qweqweqweqweqweqwe",
-          vaccinationStatus: true,
-          vaccinationTypes: { rabies: false, mother: false, dad: true },
-          medicalRecords:
-            "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
-        },
-        {
-          type: "dog",
-          name: "Bobby",
-          breed: "English Bull Terrier",
-          img: "https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1175550351.jpg",
-          description: "qweqweqweqweqweqwe",
-          vaccinationStatus: true,
-          vaccinationTypes: { rabies: false, mother: false, dad: true },
-          medicalRecords:
-            "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
-        },
-        {
-          type: "parrot",
-          name: "Paulie",
-          breed: "Macaw",
-          img: "https://nationaltoday.com/wp-content/uploads/2020/08/international-cat-day.jpg",
-          description: "qweqweqweqweqweqwe",
-          vaccinationStatus: false,
-          vaccinationTypes: { rabies: false, mother: false, dad: true },
-          medicalRecords:
-            "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
-        },
-        {
-          type: "parrot",
-          name: "Paulie",
-          breed: "Macaw",
-          img: "https://nationaltoday.com/wp-content/uploads/2020/08/international-cat-day.jpg",
-          description: "qweqweqweqweqweqwe",
-          vaccinationStatus: false,
-          vaccinationTypes: { rabies: false, mother: false, dad: true },
-          medicalRecords:
-            "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
-        },
-        {
-          type: "parrot",
-          name: "Paulie",
-          breed: "Macaw",
-          img: "https://nationaltoday.com/wp-content/uploads/2020/08/international-cat-day.jpg",
-          description: "qweqweqweqweqweqwe",
-          vaccinationStatus: false,
-          vaccinationTypes: { rabies: false, mother: false, dad: true },
-          medicalRecords:
-            "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
-        },
-        {
-          type: "parrot",
-          name: "Paulie",
-          breed: "Macaw",
-          img: "https://nationaltoday.com/wp-content/uploads/2020/08/international-cat-day.jpg",
-          description: "qweqweqweqweqweqwe",
-          vaccinationStatus: false,
-          vaccinationTypes: { rabies: false, mother: false, dad: true },
-          medicalRecords:
-            "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
-        },
-        {
-          type: "parrot",
-          name: "Paulie",
-          breed: "Macaw",
-          img: "https://nationaltoday.com/wp-content/uploads/2020/08/international-cat-day.jpg",
-          description: "qweqweqweqweqweqwe",
-          vaccinationStatus: false,
-          vaccinationTypes: { rabies: false, mother: false, dad: true },
-          medicalRecords:
-            "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
-        },
-      ];
+  {
+    UID: "1",
+    type: "dog",
+    name: "Bruno",
+    breed: "Border Collie",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg",
+    description: "qweqweqweqweqweqwe",
+    vaccinationStatus: true,
+    vaccinationTypes: { rabies: false, mother: false, dad: true },
+    medicalRecords:
+      "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
+  },
+  {
+    UID: "2",
+    type: "cat",
+    name: "Bruce",
+    breed: "Maine Coon",
+    img: "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x2.jpg",
+    description: "qweqweqweqweqweqwe",
+    vaccinationStatus: false,
+    vaccinationTypes: { rabies: false, mother: false, dad: true },
+    medicalRecords:
+      "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
+  },
+  {
+    UID: "3",
+    type: "dog",
+    name: "Tyson",
+    breed: "Bulldog",
+    img: "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403",
+    description: "qweqweqweqweqweqwe",
+    vaccinationStatus: true,
+    vaccinationTypes: { rabies: false, mother: false, dad: true },
+    medicalRecords:
+      "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
+  },
+  {
+    UID: "4",
+    type: "dog",
+    name: "Bobby",
+    breed: "English Bull Terrier",
+    img: "https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1175550351.jpg",
+    description: "qweqweqweqweqweqwe",
+    vaccinationStatus: true,
+    vaccinationTypes: { rabies: false, mother: false, dad: true },
+    medicalRecords:
+      "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
+  },
+  {
+    UID: "5",
+    type: "parrot",
+    name: "Paulie",
+    breed: "Macaw",
+    img: "https://nationaltoday.com/wp-content/uploads/2020/08/international-cat-day.jpg",
+    description: "qweqweqweqweqweqwe",
+    vaccinationStatus: false,
+    vaccinationTypes: { rabies: false, mother: false, dad: true },
+    medicalRecords:
+      "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
+  },
+  {
+    UID: "6",
+    type: "parrot",
+    name: "Paulie",
+    breed: "Macaw",
+    img: "https://nationaltoday.com/wp-content/uploads/2020/08/international-cat-day.jpg",
+    description: "qweqweqweqweqweqwe",
+    vaccinationStatus: false,
+    vaccinationTypes: { rabies: false, mother: false, dad: true },
+    medicalRecords:
+      "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
+  },
+  {
+    UID: "7",
+    type: "parrot",
+    name: "Paulie",
+    breed: "Macaw",
+    img: "https://nationaltoday.com/wp-content/uploads/2020/08/international-cat-day.jpg",
+    description: "qweqweqweqweqweqwe",
+    vaccinationStatus: false,
+    vaccinationTypes: { rabies: false, mother: false, dad: true },
+    medicalRecords:
+      "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
+  },
+  {
+    UID: "8",
+    type: "parrot",
+    name: "Paulie",
+    breed: "Macaw",
+    img: "https://nationaltoday.com/wp-content/uploads/2020/08/international-cat-day.jpg",
+    description: "qweqweqweqweqweqwe",
+    vaccinationStatus: false,
+    vaccinationTypes: { rabies: false, mother: false, dad: true },
+    medicalRecords:
+      "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
+  },
+  {
+    UID: "9",
+    type: "parrot",
+    name: "Paulie",
+    breed: "Macaw",
+    img: "https://nationaltoday.com/wp-content/uploads/2020/08/international-cat-day.jpg",
+    description: "qweqweqweqweqweqwe",
+    vaccinationStatus: false,
+    vaccinationTypes: { rabies: false, mother: false, dad: true },
+    medicalRecords:
+      "https://pdfhost.io/edit?doc=8e8d30dd-9785-4a14-a0ed-b165d2415f24",
+  },
+];
 
-export default Data
+export default Data;
--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useState, useEffect } from "react";
 
 //Card detail imports
 import Card from "@mui/material/Card";
@@ -11,24 +11,27 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Avatar from "@mui/material/Avatar";
 import { red } from "@mui/material/colors";
-import { Link } from "@mui/material";
+import { Link, MenuList } from "@mui/material";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Tooltip from "@mui/material/Tooltip";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 //table components
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 //All the icons used
 import IconButton from "@mui/material/IconButton";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import EditIcon from "@mui/icons-material/Edit";
 import DoneOutlineIcon from "@mui/icons-material/DoneOutline";
 import CancelIcon from "@mui/icons-material/Cancel";
 
@@ -36,26 +39,43 @@ import CancelIcon from "@mui/icons-material/Cancel";
 import Data from "./Data";
 
 const PetCard = () => {
-  
-  const pets = Data; //can be data
-  
+  const [pets, setPets] = useState(Data); //can be data from DB
+
+  const handleItemRemove = (UID) => {
+    console.log("delete", UID);
+    const newList = pets.filter((item) => {
+      return item.UID !== UID;
+    });
+    setPets(newList);
+    console.log(newList);
+  };
+
+  const handleItemEdit = (UID) => {
+    console.log(UID);
+    const newList = pets.filter((item) => {
+      return item.UID === UID;
+    });
+    setPets(newList);
+    console.log(newList);
+  };
+
+  useEffect(() => {
+    console.log(pets);
+  }, []);
   //in the future can use mapping function to map through database. Render data with useEffect.
   return (
     <div>
-      <Grid
-        container
-        spacing={3}
-      >
+      <Grid container spacing={3}>
         {pets.map((elem) => (
           <Grid
             item
-            sx={{ marginTop: "20px", marginBottom: "20px"}}
+            sx={{ marginTop: "20px", marginBottom: "20px" }}
             xs={12}
             sm={6}
             md={4}
             key={pets.indexOf(elem)}
           >
-            <Card elevation={5} style={{borderRadius:'30'}}>
+            <Card elevation={5} style={{ borderRadius: "30" }}>
               <CardHeader
                 avatar={
                   <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -63,13 +83,29 @@ const PetCard = () => {
                   </Avatar>
                 }
                 action={
-                  <IconButton aria-label="settings">
-                    <MoreVertIcon />
-                  </IconButton>
+                  <Box sx={{ flexGrow: 0 }}>
+                    <Tooltip title="Click To Edit">
+                      <IconButton
+                        aria-label="settings"
+                        onClick={() => handleItemEdit(elem.UID)}
+                      >
+                        <EditIcon />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Click To Remove">
+                      <IconButton
+                        aria-label="settings"
+                        onClick={() => handleItemRemove(elem.UID)}
+                      >
+                        <DeleteOutlinedIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </Box>
                 }
                 title={`${elem.name}`}
                 subheader={`${elem.breed}`}
               />
+
               <CardMedia
                 component="img"
                 height="250"
@@ -82,57 +118,69 @@ const PetCard = () => {
                   to cook together with your guests. Add 1 cup of frozen peas
                   along with the mussels, if you like.
                 </Typography>
-                <TableContainer sx={{marginTop:'20px'}} component={Paper} elevation={3}>
-                  <Table sx={{ maxWidth: 350 }} size="small" aria-label="a dense table">
+                <TableContainer
+                  sx={{ marginTop: "20px" }}
+                  component={Paper}
+                  elevation={3}
+                >
+                  <Table
+                    sx={{ maxWidth: 350 }}
+                    size="small"
+                    aria-label="a dense table"
+                  >
                     <TableHead>
                       <TableRow>
                         <TableCell align="center">Vaccine Type</TableCell>
                         <TableCell align="center">Status</TableCell>
                       </TableRow>
                     </TableHead>
-                  <TableBody>
-                    {Object.keys(elem.vaccinationTypes).map((key, index) => {
-                      return (
-                        <TableRow key={index}>
-                          <TableCell align="center" component="th" scope="row">
-                            {key}
-                          </TableCell>
-                          <TableCell align="center" >
-                            {elem.vaccinationTypes[key] ? (
-                          <DoneOutlineIcon 
-                            sx={{
-                              marginTop: 0.5,
-                              height: "18px",
-                              color: "green",
-                            }}
-                          />
-                        ) : (
-                          <CancelIcon
-                            sx={{
-                              marginTop: 0.5,
-                              height: "18px",
-                              color: "red",
-                            }}
-                          />)}
-                          </TableCell>
-                        </TableRow>
-                        )}
-                      )}
+                    <TableBody>
+                      {Object.keys(elem.vaccinationTypes).map((key, UID) => {
+                        return (
+                          <TableRow key={UID}>
+                            <TableCell
+                              align="center"
+                              component="th"
+                              scope="row"
+                            >
+                              {key}
+                            </TableCell>
+                            <TableCell align="center">
+                              {elem.vaccinationTypes[key] ? (
+                                <DoneOutlineIcon
+                                  sx={{
+                                    marginTop: 0.5,
+                                    height: "18px",
+                                    color: "green",
+                                  }}
+                                />
+                              ) : (
+                                <CancelIcon
+                                  sx={{
+                                    marginTop: 0.5,
+                                    height: "18px",
+                                    color: "red",
+                                  }}
+                                />
+                              )}
+                            </TableCell>
+                          </TableRow>
+                        );
+                      })}
                     </TableBody>
                   </Table>
                 </TableContainer>
 
-                  <Typography fontSize="24" sx={{paddingTop:'20px'}}>
-                    Medical Records: {`${" "}`}
-                    <Link
-                      target="_blank"
-                      rel="noopener"
-                      href={`${elem.medicalRecords}`}
-                    >
-                      View Here
-                    </Link>
-                  </Typography>
-
+                <Typography fontSize="24" sx={{ paddingTop: "20px" }}>
+                  Medical Records: {`${" "}`}
+                  <Link
+                    target="_blank"
+                    rel="noopener"
+                    href={`${elem.medicalRecords}`}
+                  >
+                    View Here
+                  </Link>
+                </Typography>
               </CardContent>
             </Card>
           </Grid>
